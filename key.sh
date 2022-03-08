4911955cb2270ea0dd80a829630aa47406c03b9b
MasterS00 <99988647+MasterS00@users.noreply.github.com>
2022-03-08 01:28:03 +0000

    Add files via upload

---
+++ key.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+xdotool key b+a; xdotool key s+h; xdotool key space; xdotool key period; xdotool key shift+7; xdotool key r+e; xdotool key v+e; xdotool key r+s; xdotool key e;
+xdotool key shift+7; xdotool key s+e; xdotool key t+u; xdotool key p; xdotool key period; xdotool key s+h; xdotool key Return
